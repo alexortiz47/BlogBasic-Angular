af48ba3bb7ae1ea7f035aa5772de721980bfb355
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-06 09:39:11 +0100

    Routing url para componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: BlogComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'new', component: FormularioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
