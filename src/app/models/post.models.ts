d0e817227e3a3daf2c9c8e2390af26601e813fd4
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-06 10:32:27 +0100

    Modelo Post con fecha terminado

--- src/app/models/post.models.ts
+++ src/app/models/post.models.ts
@@ -4,16 +4,16 @@ export class Post {
   texto: string;
   autor: string;
   imagen: string;
-  fecha: number;
+  fecha: string;
   categoria: string;
 
-  constructor(pTitulo: string, pTexto: string, pAutor: string, pImagen: string, pFecha: number, pCategoria: string) {
+  constructor(pTitulo: string, pTexto: string, pAutor: string, pImagen: string, pCategoria: string) {
 
     this.titulo = pTitulo;
     this.texto = pTexto;
     this.autor = pAutor;
     this.imagen = pImagen;
-    this.fecha = pFecha;
+    this.fecha = `${new Date().getDate()} / ${new Date().getMonth()} / ${new Date().getFullYear()}`;
     this.categoria = pCategoria;
 
   }
